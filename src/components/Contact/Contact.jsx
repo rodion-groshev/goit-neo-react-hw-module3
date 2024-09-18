f2e6ff86a81e8eea6a4bf71022cbe3e99ac0132e
Rodion <rodion.groshev@gmail.com>
2024-09-19 01:17:50 +0200

    hw_03 first try

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import css from "./Contact.module.css";
+
+const Contact = ({ contact, handleClick }) => {
+  return (
+    <div className={css.card}>
+      <div className={css.cardOutline}>
+        <div className={css.cardWrapper}>
+          <svg width={15} height={15}>
+            <use href="/src/img/icons.svg#icon-name"></use>
+          </svg>
+          <p>{contact.name}</p>
+        </div>
+        <div className={css.cardWrapper}>
+          <svg width={15} height={15}>
+            <use href="/src/img/icons.svg#icon-phone"></use>
+          </svg>
+          <p>{contact.number}</p>
+        </div>
+      </div>
+      <button
+        className={css.contactBtn}
+        onClick={() => handleClick(contact.id)}
+        type="click"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Contact;
