af0aa71de47e473990254c03e04ee8bc9060c9b7
Rodion <rodion.groshev@gmail.com>
2024-09-21 11:50:28 +0200

    one

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,13 +6,13 @@ const Contact = ({ contact, handleClick }) => {
       <div className={css.cardOutline}>
         <div className={css.cardWrapper}>
           <svg className={css.icon} width={15} height={15}>
-            <use href="/src/img/icons.svg#icon-user" />
+            <use xlinkHref="/src/img/icons.svg#icon-user"></use>
           </svg>
           <p>{contact.name}</p>
         </div>
         <div className={css.cardWrapper}>
           <svg className={css.icon} width={15} height={15}>
-            <use href="/src/img/icons.svg#icon-phone" />
+            <use xlinkHref="/src/img/icons.svg#icon-phone"></use>
           </svg>
           <p>{contact.number}</p>
         </div>
