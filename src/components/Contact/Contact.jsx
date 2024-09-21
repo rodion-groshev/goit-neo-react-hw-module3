f3020941a5451ad4b2c1850bd83569a608e94f99
Rodion <rodion.groshev@gmail.com>
2024-09-21 14:35:02 +0200

    finnaly

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,17 @@
 import css from "./Contact.module.css";
+import iconUser from "../../img/user.svg";
+import iconPhone from "../../img/phone.svg";
 
 const Contact = ({ contact, handleClick }) => {
   return (
     <div className={css.card}>
       <div className={css.cardOutline}>
         <div className={css.cardWrapper}>
-          <svg className={css.icon} width={15} height={15}>
-            <use xlinkHref="/src/img/icons.svg#icon-user"></use>
-          </svg>
+          <img src={iconUser}></img>
           <p>{contact.name}</p>
         </div>
         <div className={css.cardWrapper}>
-          <svg className={css.icon} width={15} height={15}>
-            <use xlinkHref="/src/img/icons.svg#icon-phone"></use>
-          </svg>
+          <img src={iconPhone}></img>
           <p>{contact.number}</p>
         </div>
       </div>
